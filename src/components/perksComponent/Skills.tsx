cc52553b37e3e81dbb54555d29e0268b8058301b
MelvinMelendrez <melvin_sss@hotmail.com>
2024-07-21 22:58:31 -0700

    design update

--- src/components/perksComponent/Skills.tsx
+++ src/components/perksComponent/Skills.tsx
@@ -9,12 +9,14 @@ const Skills = () => {
             <span className={Styles.Title}>my skills</span>
             <p>I have developed a diverse skill set throughout my career, allowing me to tackle a wide range of projects</p>
             <div className={Styles.cards}>
-                {SkillCards.map((card, index) => (
-                    <div key={index} className={Styles.card}>
-                        <img src={card.image} alt={`${card.text} icon`} className={Styles.skillIcon} />
-                        <span>{card.text}</span>
-                    </div>
-                ))}
+                <div>
+                    {SkillCards.map((card, index) => (
+                        <div key={index} className={Styles.card}>
+                            <img src={card.image} alt={`${card.text} icon`} className={Styles.skillIcon} />
+                            <span>{card.text}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
