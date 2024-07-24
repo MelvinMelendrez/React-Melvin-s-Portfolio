b9780e833cf678d5546469adc002e49c37ec6fbd
MelvinMelendrez <melvin_sss@hotmail.com>
2024-07-23 23:28:12 -0700

    add contact me buttons

--- src/components/perksComponent/ContactMe.tsx
+++ src/components/perksComponent/ContactMe.tsx
@@ -8,36 +8,47 @@ const ContactForm: React.FC = () => {
         return <CongratulationMessage />;
     }
     return (
-        <form onSubmit={handleSubmit} className={Styles.form}>
-            <label htmlFor="email">
-                email address
-            </label>
-            <input
-                id="email"
-                type="email"
-                name="email"
-                required
-            />
-            <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-            />
-            <label>massage</label>
-            <textarea
-                id="message"
-                name="message"
-                className={Styles.textarea}
-            />
-            <ValidationError
-                prefix="Message"
-                field="message"
-                errors={state.errors}
-            />
-            <button type="submit" disabled={state.submitting}>
-                Submit
-            </button>
-        </form>
+        <div className={Styles.ContactMeBody}>
+            <div className={Styles.buttons}>
+                <button onClick={() => window.open('https://github.com/MelvinMelendrez', "_blank")}>
+                    github
+                </button>
+                <button onClick={() => window.open('https://www.linkedin.com/in/melvinmelendrez/', "_blank")}>
+                    linkedin
+                </button>
+            </div>
+
+            <form onSubmit={handleSubmit} className={Styles.form1}>
+                <label htmlFor="email">
+                    email address
+                </label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    required
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+                <label>massage</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    className={Styles.textarea}
+                />
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
+                <button type="submit" disabled={state.submitting}>
+                    Submit
+                </button>
+            </form>
+        </div>
     );
 }
 
